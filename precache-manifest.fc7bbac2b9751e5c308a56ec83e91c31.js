f8a89887a24817b390b4a3aec76c9d6f1adfd364
poonasp257 <poonasp257@gmail.com>
2019-05-03 02:21:15 +0900

    Updates

---
+++ precache-manifest.fc7bbac2b9751e5c308a56ec83e91c31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542717c3399a80531c4695537f1ce0ff",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "abdbe136366baee5a585",
+    "url": "/Portfolio/static/css/main.475112af.chunk.css"
+  },
+  {
+    "revision": "9989a348abb82d3fff8b",
+    "url": "/Portfolio/static/js/2.09735bde.chunk.js"
+  },
+  {
+    "revision": "abdbe136366baee5a585",
+    "url": "/Portfolio/static/js/main.740df0fa.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "6f7591616b8f7bbb6bc74a304935d119",
+    "url": "/Portfolio/static/media/ico_github.6f759161.svg"
+  },
+  {
+    "revision": "7c524b3545d5e110c24385e3778d20eb",
+    "url": "/Portfolio/static/media/ico_mail.7c524b35.svg"
+  }
+]);
